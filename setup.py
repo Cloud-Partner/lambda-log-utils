42e8740fad8dd34d2bd4759774a6d1248594c5fd
Keiichi Nakayama <nakayama@chara-web.co.jp>
2020-06-01 14:45:37 +0900

    chore:

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="lambda-log-utils",
+    name="lambda_log_utils",
     version="0.1",
     install_requires=[
         "aws-lambda-powertools==0.8.1"
